e632bc0314d5651ff2600302c788b9b25f475808
이은정 <ej2grateful@gmail.com>
2025-08-05 11:29:56 +0900

    ready

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const App = () => {
+  return (
+    <div className="bg-sky-200 min-h-screen flex justify-center items-center">
+      App
+    </div>
+  );
+};
+
+export default App;
